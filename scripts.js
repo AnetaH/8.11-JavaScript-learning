a642da6d72fea93f74887b036faefdef3d93fb61
Aneta Halz <aneta.halz@interia.pl>
2017-03-02 23:11:21 +0100

    Console.log deletion

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 var list = document.getElementById('list');
-console.log(list);
 
 var add = document.getElementById('addElem');
-console.log(add);
 
 add.addEventListener('click', function() {
   list.innerHTML += '<li>item ' + document.getElementsByTagName('li').length + '</li>';
